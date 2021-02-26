1b222f426b3bbcfd0f45a114356ba0a9cba334ff
MBrandt <47629715+mbrndt@users.noreply.github.com>
2021-02-26 10:27:40 +0100

    adding google auth

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-console.log(firebase)
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('whenSignedIn');
+const signOurBtn = document.getElementById('whenSignedOut');
+
+const userDetails = document.getElementById('userDetails');
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
